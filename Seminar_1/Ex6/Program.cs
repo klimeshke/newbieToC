7e7c227621d860b1586936bec10c52f3dbdadfc5
klimeshke <klimenkojk@yandex.ru>
2023-03-17 09:05:22 +0300

    Ex6 - Done and remove folder Ex

---
+++ Seminar_1/Ex6/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли оно четным
+Console.Clear();
+
+Console.Write("Введите целое число: ");
+int num = int.Parse(Console.ReadLine() ?? "");
+
+if (num % 2 == 0)
+    Console.WriteLine("Число четное!");
+else
+    Console.WriteLine("Число НЕчетное!");
