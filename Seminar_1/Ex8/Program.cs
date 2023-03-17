1fc2531a618b92b9ebf9264c4c83929be32f66c7
klimeshke <klimenkojk@yandex.ru>
2023-03-17 09:29:55 +0300

    Ex8 - Done

---
+++ Seminar_1/Ex8/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показвает все чётные числа от 1 до N
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine() ?? "");
+int count = 2;
+
+while (count <= num)
+{
+    Console.Write($"{count} ");
+    count += 2;
+}
