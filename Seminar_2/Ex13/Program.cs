1e69c30f20f18d5c63eb09386abd4a7443caa172
klimeshke <klimenkojk@yandex.ru>
2023-03-19 12:14:28 +0300

    Ex13 - Done

---
+++ Seminar_2/Ex13/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+
+Console.Write("Введите трёхзначное число: ");
+int num = int.Parse(Console.ReadLine() ?? "");
+
+int check3 = (num / 100);
+int a3 = ((num % 100) % 10);
+
+if (check3 == 0)
+    Console.WriteLine("Третьей цифры нет");
+else Console.WriteLine($"Третья цифра в числе {num} -> {a3}");
