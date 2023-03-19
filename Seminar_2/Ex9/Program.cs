fb22c25cb86478e393aa397cdb2bdfd00d1578c6
klimeshke <klimenkojk@yandex.ru>
2023-03-19 11:35:11 +0300

    Ex10 - Done

---
+++ Seminar_2/Ex9/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [100, 999] и показывает наибольшую цифру числа.
+Console.Clear();
+
+int num = new Random().Next(100, 1000);
+int a1 = num / 100;
+int a2 = ((num / 10) % 10);
+int a3 = ((num % 100) % 10);
+int max = a1;
+if (max < a2)
+    max = a2;
+if (max < a3)
+    max = a3;
+
+Console.WriteLine($"Максимальная цифра в числе {num} -> {max}");
---
+++ Seminar_2/doubleVarA/Program.cs
@@ -0,0 +1,12 @@
+﻿// Циклический ввод переменной типа double
+
+double varA;
+
+while (true)
+{
+    Console.Write("Введите А: ");
+    if (double.TryParse(Console.ReadLine(), out varA)) // TryParse - попыткапарсить, out - вывод наружу в переменную
+        break;
+    Console.WriteLine("Ошибка ввода!");
+}
+Console.WriteLine($"A = {varA}");
