5c1a22663e2fa2b7f480df84318949dd956ba4d3
klimeshke <klimenkojk@yandex.ru>
2023-03-20 13:06:44 +0300

    Ex11-12 - add

---
+++ Seminar_2/Ex11/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этото числа.
+Console.Clear();
+
+int num = new Random().Next(100, 1000);
+int a1 = num / 100;
+int a3 = num % 10;
+
+int result = a1 * 10 + a3;
+
+Console.WriteLine($"Из числа {num} получаем {result}");
---
+++ Seminar_2/Ex12/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому
+// Если второе число не кратно превому числу, то программа выводит остаток от деления.
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine() ?? "");
+
+if (num1 % num2 == 0)
+    Console.WriteLine($"Кратно");
+else
+{
+    int rem = num1 % num2;
+    Console.WriteLine($"Не кратно. Остаток от деления -> {rem}");
+}
