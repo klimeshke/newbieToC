fa2702ae0887831450595ee4075b2015925608a0
klimeshke <klimenkojk@yandex.ru>
2023-03-20 13:52:38 +0300

    Ex14-16 - add

---
+++ Seminar_2/Ex14/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine() ?? "");
+
+// bool result = (num % 7 == 0 && num % 23 == 0); ---- bool сохраняет true false
+// if (result)                                ___/
+// {
+
+if (num % 7 == 0 && num % 23 == 0)
+    Console.WriteLine($"Число {num} кратно 7 и 23");
+else
+    Console.WriteLine($"Число {num} НЕ кратно");
---
+++ Seminar_2/Ex16/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли одно число квадратом другого
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine() ?? "");
+
+bool result = (num1 == num2 * num2 || num2 == num1 * num1);
+
+if (result)
+    Console.WriteLine($"{num1}, {num2} -> ДА");
+else
+    Console.WriteLine($"{num1}, {num2} -> НЕТ");
